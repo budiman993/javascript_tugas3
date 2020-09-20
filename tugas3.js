a5d8d3920d1ffc745ac8413ecaa4ac1d4ea9283d
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-09-20 15:40:41 +0700

    Add files via upload

---
+++ tugas3.js
@@ -0,0 +1,11 @@
+var nama = "Nama Pertama"
+
+function panggilNama(pNama){
+	var nama = "Nama Kedua"
+	return nama
+}
+
+panggilNama("Nama Pertama")
+
+console.log("Sebelum diubah :", nama)
+console.log("Setelah diubah :",panggilNama())
